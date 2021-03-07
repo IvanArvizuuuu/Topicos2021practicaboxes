77113bd148ee3b611c1a3c841d516aae6708e86e
IvanArvizuuuu <79930523+IvanArvizuuuu@users.noreply.github.com>
2021-03-07 16:21:02 -0600

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,45 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private HBox hBox;
+    private Button btn1, btn2, btn3;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        CrearUI(); // se llama al hbox
+        primaryStage.setTitle("Proyecto TAP");
+        primaryStage.setScene(new Scene(hBox, 300, 275));
+        primaryStage.show();
+    }
+
+    private void CrearUI(){
+
+        btn1 = new Button ("Boton 1");
+        btn2 = new Button ("Boton 2");
+        btn3 = new Button ("Boton 3");
+
+        hBox = new HBox();
+        hBox.getChildren().addAll(btn1,btn2,btn3);
+        hBox.setSpacing(10);
+        hBox.setPadding(new Insets(20,20,20,20));
+
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
